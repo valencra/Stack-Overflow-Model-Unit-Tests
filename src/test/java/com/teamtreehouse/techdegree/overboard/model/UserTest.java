8739be92640e5ed503fd4133673c05905047ca71
valencra <valencra@mcmaster.ca>
2017-10-15 20:00:35 -0400

    Added questioner, answerer, and voter user objects to setUp method

--- src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -8,17 +8,21 @@ import static org.junit.Assert.*;
 public class UserTest {
 
     private Board board;
+    private User questioner;
+    private User answerer;
+    private User voter;
 
     @Before
     public void setUp() throws Exception {
         board = new Board("Java");
+        questioner = new User(board, "questioner");
+        answerer = new User(board, "answerer");
+        voter = new User(board, "voter");
     }
 
     @Test
     public void upvotingQuestionRaisesQuestionerReputationBy5() throws Exception {
-        User questioner = new User(board, "questioner");
         Post question = questioner.askQuestion("What is Java?");
-        User voter = new User(board, "voter");
 
         voter.upVote(question);
 
